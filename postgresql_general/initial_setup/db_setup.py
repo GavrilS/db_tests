3c2bc4f17fe41972a97e28f9c94554c392c05b7c
GavrilS <gavr1lll@abv.bg>
2023-12-12 23:56:50 +0200

    WIP: added functionality to connect to db and execute sql file to set up db

--- postgresql_general/initial_setup/db_setup.py
+++ postgresql_general/initial_setup/db_setup.py
@@ -1,7 +1,9 @@
 import os
-# import psycopg2
+import psycopg2
 
 DEFAULT_ENV_FILE = 'docker_postgresql_general/.env'
+DEFAULT_DB_HOST = 'localhost'
+DEFAULT_DB_PORT = '5433'
 
 
 def load_env_from_file(env_file=DEFAULT_ENV_FILE):
@@ -18,7 +20,37 @@ def load_env_from_file(env_file=DEFAULT_ENV_FILE):
 
 
 def db_connect(db_name, db_user, db_pass):
-    pass
+    try:
+        conn = psycopg2.connect(
+            host=DEFAULT_DB_HOST,
+            database=db_name,
+            user=db_user,
+            password=db_pass,
+            port=DEFAULT_DB_PORT
+        )
+        print('Connection set up successfully!!!')
+        return conn
+    except Exception as e:
+        print('There was an error trying to connect to db: ', str(e))
+        exit()
+
+
+def execute_db_setup(conn):
+    try:
+        cur = conn.cursor()
+        cur.execute(open("test_db_schema.sql", "r").read())
+        cur.close()
+        conn.commit()
+        print('Operations executed and commited!')
+    except Exception as e:
+        print('There was an error trying to execute to set up .sql commands: ', str(e))
+    finally:
+        close_db_connection(conn)
+
+
+def close_db_connection(conn):
+    conn.close()
+    print('Connection closed!')
 
 
 if __name__=='__main__':
@@ -30,3 +62,6 @@ if __name__=='__main__':
     print('db_name: ', db_name)
     print('db_user: ', db_user)
     print('db_pass: ', db_pass)
+
+    conn = db_connect(db_name, db_user, db_pass)
+    execute_db_setup(conn)
